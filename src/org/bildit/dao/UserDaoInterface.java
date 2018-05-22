b64cdb1d1467d270c188fc1251d756ce19e7c732
Boris Nunic <boris.nunic88@gmail.com>
2018-05-22 14:45:04 +0200

    UserDao interface edited

--- src/org/bildit/dao/UserDaoInterface.java
+++ src/org/bildit/dao/UserDaoInterface.java
@@ -1,7 +1,6 @@
 package org.bildit.dao;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Map;
 ;
 
@@ -11,13 +10,15 @@ public interface UserDaoInterface {
 	
 	public void editUser(String username) throws SQLException;
 	
-	public ArrayList<String> getUsernames() throws SQLException;
+	public boolean validateUser(String username, String password) throws SQLException;
 	
 	public void deleteUser(String username) throws SQLException;
 	
-	public Map<Integer, String> getLeaderboard() throws SQLException;
+	public Map<String, Integer> getLeaderboard() throws SQLException;
 	
 	public void resetLeaderboard() throws SQLException;
+	
+	public void addScore(String username, int score) throws SQLException;
 	
 	
 	
