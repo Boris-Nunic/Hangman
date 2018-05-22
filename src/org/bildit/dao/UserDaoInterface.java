61814288923bd7cea29e6a9b90d056fcb647b161
Maja Vasilic <rmaja7@gmail.com>
2018-05-22 02:16:46 +0200

    DB dodana, testirana; implementirani user i words dao interfejsi

--- src/org/bildit/dao/UserDaoInterface.java
+++ src/org/bildit/dao/UserDaoInterface.java
@@ -1,19 +1,21 @@
 package org.bildit.dao;
 
-import java.sql.SQLException;
+import java.sql.SQLException; 
 import java.util.ArrayList;
 import java.util.Map;
-;
+
+import org.bildit.beans.User;
+
 
 public interface UserDaoInterface {
 
 	public void addUser(String username, String password) throws SQLException;
 	
-	public void editUser(String username) throws SQLException;
+	public void editUser(User user) throws SQLException;
 	
 	public ArrayList<String> getUsernames() throws SQLException;
 	
-	public void deleteUser(String username) throws SQLException;
+	public void deleteUser(User user) throws SQLException;
 	
 	public Map<Integer, String> getLeaderboard() throws SQLException;
 	
